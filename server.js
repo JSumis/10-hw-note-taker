5928f2a53a0f758038f3f9f40453785b5af380ba
Joseph Sumis <Jsumis@comcast.net>
2021-06-22 15:21:01 -0500

    updated dirname on line 21 server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 
 
